37a5802675e01cab9423b560b6973de4b48cd902
Sander Marechal <sander@prezent.nl>
2012-01-02 16:28:19 +0100

    Add support for WHERE NOT queries

--- tests/Pagerfanta/Tests/Adapter/DoctrineORM/WhereInWalkerTest.php
+++ tests/Pagerfanta/Tests/Adapter/DoctrineORM/WhereInWalkerTest.php
@@ -103,4 +103,18 @@ class WhereInWalkerTest extends DoctrineORMTestCase
                 "SELECT u0_.id AS id0, g1_.id AS id1 FROM User u0_ INNER JOIN user_group u2_ ON u0_.id = u2_.user_id INNER JOIN groups g1_ ON g1_.id = u2_.group_id WHERE (1 = 1 AND 2 = 2 OR 3 = 3) AND u0_.id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $whereInQuery->getSql()
         );
     }
+
+    public function testWhereInQuery_WhereNot()
+    {
+        $query = $this->entityManager->createQuery(
+                        'SELECT u, g FROM Pagerfanta\Tests\Adapter\DoctrineORM\User u JOIN u.groups g WHERE NOT 1 = 2'
+        );
+        $whereInQuery = clone $query;
+        $whereInQuery->setHint(Query::HINT_CUSTOM_TREE_WALKERS, array('Pagerfanta\Adapter\DoctrineORM\WhereInWalker'));
+        $whereInQuery->setHint('id.count', 10);
+
+        $this->assertEquals(
+                "SELECT u0_.id AS id0, g1_.id AS id1 FROM User u0_ INNER JOIN user_group u2_ ON u0_.id = u2_.user_id INNER JOIN groups g1_ ON g1_.id = u2_.group_id WHERE (NOT 1 = 2) AND u0_.id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $whereInQuery->getSql()
+        );
+    }
 }
