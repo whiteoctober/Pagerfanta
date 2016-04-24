7ca5bf79a7307d81bd5dfc29ed69770ceaf04028
Vitor Mattos <vitor.mattos@phprio.org>
2016-04-24 20:06:43 -0300

    New template for Telegram Inline paginator

---
+++ src/Pagerfanta/View/TelegramInlineView.php
@@ -0,0 +1,256 @@
+<?php
+namespace Pagerfanta\View;
+use Pagerfanta\View\ViewInterface;
+use Pagerfanta\PagerfantaInterface;
+use Pagerfanta\View\Template\TemplateInterface;
+use TelegramPagerfanta\View\Template\TelegramInlineTemplate;
+
+class TelegramInlineView implements ViewInterface
+{
+    /**
+     * @var TelegramInlineTemplate
+     */
+    private $template;
+
+    /**
+     * @var PagerfantaInterface
+     */
+    private $pagerfanta;
+
+    private $currentPage;
+    private $nbPages;
+
+    private $startPage;
+    private $endPage;
+    private $maxPerPage;
+    
+    private $buttons;
+
+    public function __construct(TemplateInterface $template = null)
+    {
+        $this->template = $template ?: $this->createDefaultTemplate();
+        $this->buttons = new \stdClass();
+    }
+
+    protected function createDefaultTemplate()
+    {
+        return new \Pagerfanta\View\Template\TelegramInlineTemplate();
+    }
+
+    public function render(PagerfantaInterface $pagerfanta, $routeGenerator, array $options = array())
+    {
+        $this->initializePagerfanta($pagerfanta);
+        $this->initializeOptions($options);
+
+        $this->configureTemplate($routeGenerator, $options);
+
+        return $this->generatePages();
+    }
+
+    private function initializePagerfanta(PagerfantaInterface $pagerfanta)
+    {
+        $this->pagerfanta = $pagerfanta;
+
+        $this->currentPage = $pagerfanta->getCurrentPage();
+        $this->nbPages = $pagerfanta->getNbPages();
+        $this->maxPerPage = $pagerfanta->getMaxPerPage();
+    }
+
+    private function initializeOptions($options)
+    {
+        $this->proximity = isset($options['proximity']) ?
+                           (int) $options['proximity'] :
+                           $this->getDefaultProximity();
+    }
+
+    protected function getDefaultProximity()
+    {
+        $proximityEstimated = floor($this->maxPerPage / 2);
+        if($proximityEstimated < 2) {
+            $proximity = $proximityEstimated;
+        } else {
+            $proximity = 2;
+        }
+        return $proximity;
+    }
+
+    private function configureTemplate($routeGenerator, $options)
+    {
+        $this->template->setRouteGenerator($routeGenerator);
+        $this->template->setOptions($options);
+    }
+
+    private function generatePages()
+    {
+        $this->calculateStartAndEndPage();
+
+        $this->first();
+        if($this->currentPage > $this->maxPerPage) {
+            $this->previous();
+            if($this->nbPages < $this->endPage) {
+                $this->startPage++;
+            }
+        }
+        if($this->pagerfanta->hasNextPage() && $this->currentPage >= $this->maxPerPage && $this->nbPages - $this->endPage >= 1) {
+            $this->endPage--;
+            $this->pages();
+        } else {
+            $this->pages();
+        }
+        $this->next();
+        if($this->pagerfanta->hasNextPage() && $this->currentPage >= $this->maxPerPage && $this->nbPages > $this->endPage) {
+            $this->endPage++;
+        }
+        $this->last();
+        return (string)$this;
+    }
+
+    private function calculateStartAndEndPage()
+    {
+        $startPage = $this->currentPage - $this->proximity;
+        $endPage = $this->currentPage + $this->proximity;
+
+        if ($this->startPageUnderflow($startPage)) {
+            $startPage = 1;
+            $endPage = $this->maxPerPage;
+            $endPage = $this->calculateEndPageForStartPageUnderflow($startPage, $endPage);
+        }
+        if ($this->endPageOverflow($endPage)) {
+            $startPage = $this->calculateStartPageForEndPageOverflow($startPage, $endPage);
+            $endPage = $this->nbPages;
+        }
+
+        $this->startPage = $startPage;
+        $this->endPage = $endPage;
+    }
+
+    private function startPageUnderflow($startPage)
+    {
+        return $startPage < 1;
+    }
+
+    private function endPageOverflow($endPage)
+    {
+        return $endPage > $this->nbPages;
+    }
+
+    private function calculateEndPageForStartPageUnderflow($startPage, $endPage)
+    {
+        return min($endPage + (1 - $startPage), $this->nbPages);
+    }
+
+    private function calculateStartPageForEndPageOverflow($startPage, $endPage)
+    {
+        return max($startPage - ($endPage - $this->nbPages), 1);
+    }
+
+    private function previous()
+    {
+        if ($this->currentPage > $this->maxPerPage) {
+            if($this->nbPages >= $this->endPage) {
+                $this->pushNewButton(
+                    $this->template->previousEnabled($this->startPage - 1)
+                );
+            } else {
+                $this->pushNewButton(
+                    $this->template->previousEnabled($this->startPage)
+                );
+            }
+        }
+    }
+
+    private function first()
+    {
+        if ($this->startPage > 1) {
+            if($this->currentPage > $this->maxPerPage) {
+                $this->pushNewButton(
+                    $this->template->first()
+                );
+            } else {
+                $this->pushNewButton(
+                    $this->template->page(1)
+                );
+            }
+        }
+    }
+
+    private function pages()
+    {
+        foreach (range($this->startPage, $this->endPage) as $page) {
+            $this->page($page);
+        }
+    }
+
+    private function page($page)
+    {
+        if ($page == $this->currentPage) {
+            $this->pushNewButton(
+                $this->template->current($page)
+            );
+        } else {
+            $this->pushNewButton(
+                $this->template->page($page)
+            );
+        }
+    }
+    
+    private function pushNewButton(array $params) {
+        $this->buttons->inline_keyboard[0][] = (object)$params;
+    }
+
+    private function toLast($n)
+    {
+        return $this->pagerfanta->getNbPages() - ($n - 1);
+    }
+
+    private function last()
+    {
+        if ($this->pagerfanta->getNbPages() > $this->endPage) {
+            if($this->nbPages - $this->endPage <= 1) {
+                $this->pushNewButton(
+                    $this->template->page($this->pagerfanta->getNbPages())
+                );
+            } else {
+                $this->pushNewButton(
+                    $this->template->last($this->pagerfanta->getNbPages())
+                );
+            }
+        }
+    }
+
+    private function next()
+    {
+        if ($this->pagerfanta->hasNextPage()) {
+            if($this->nbPages - $this->endPage <= 2) {
+                if($this->nbPages - $this->endPage == 1) {
+                    $this->pushNewButton(
+                        $this->template->page($this->endPage + 1)
+                    );
+                }
+            } else {
+                $this->pushNewButton(
+                    $this->template->nextEnabled($this->endPage + 1)
+                );
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'TelegramInline';
+    }
+    
+
+    /**
+     * Convert the collection to its string representation.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return json_encode($this->buttons, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    }
+}
---
+++ tests/Pagerfanta/Tests/View/TelegramInlineViewTest.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Pagerfanta\Tests\View;
+
+use Pagerfanta\View\TelegramInlineView;
+
+class TelegramInlineViewTest extends ViewTestCase
+{
+    protected function createView()
+    {
+        return new TelegramInlineView();
+    }
+
+    public function testRenderNormal()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(10);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"1","callback_data":"|1|"},{"text":"8","callback_data":"|8|"},{"text":"9","callback_data":"|9|"},{"text":"· 10 ·","callback_data":"|10|"},{"text":"11","callback_data":"|11|"},{"text":"12 ›","callback_data":"|12|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderFirstPage()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(1);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"· 1 ·","callback_data":"|1|"},{"text":"2","callback_data":"|2|"},{"text":"3","callback_data":"|3|"},{"text":"4","callback_data":"|4|"},{"text":"5","callback_data":"|5|"},{"text":"6","callback_data":"|6|"},{"text":"7","callback_data":"|7|"},{"text":"8","callback_data":"|8|"},{"text":"9","callback_data":"|9|"},{"text":"10","callback_data":"|10|"},{"text":"11 ›","callback_data":"|11|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderLastPage()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(100);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"« 1","callback_data":"|1|"},{"text":"‹ 95","callback_data":"|95|"},{"text":"96","callback_data":"|96|"},{"text":"97","callback_data":"|97|"},{"text":"98","callback_data":"|98|"},{"text":"99","callback_data":"|99|"},{"text":"· 100 ·","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderWhenStartProximityIs2()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(4);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"1","callback_data":"|1|"},{"text":"2","callback_data":"|2|"},{"text":"3","callback_data":"|3|"},{"text":"· 4 ·","callback_data":"|4|"},{"text":"5","callback_data":"|5|"},{"text":"6","callback_data":"|6|"},{"text":"7 ›","callback_data":"|7|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderWhenStartProximityIs3()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(5);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"1","callback_data":"|1|"},{"text":"3","callback_data":"|3|"},{"text":"4","callback_data":"|4|"},{"text":"· 5 ·","callback_data":"|5|"},{"text":"6","callback_data":"|6|"},{"text":"7","callback_data":"|7|"},{"text":"8 ›","callback_data":"|8|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderWhenEndProximityIs2FromLast()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(97);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"« 1","callback_data":"|1|"},{"text":"‹ 94","callback_data":"|94|"},{"text":"95","callback_data":"|95|"},{"text":"96","callback_data":"|96|"},{"text":"· 97 ·","callback_data":"|97|"},{"text":"98","callback_data":"|98|"},{"text":"100","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderWhenEndProximityIs3FromLast()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(96);
+
+        $options = array();
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"« 1","callback_data":"|1|"},{"text":"‹ 93","callback_data":"|93|"},{"text":"94","callback_data":"|94|"},{"text":"95","callback_data":"|95|"},{"text":"· 96 ·","callback_data":"|96|"},{"text":"97","callback_data":"|97|"},{"text":"98 ›","callback_data":"|98|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderModifyingProximity()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(10);
+
+        $options = array('proximity' => 3);
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"1","callback_data":"|1|"},{"text":"7","callback_data":"|7|"},{"text":"8","callback_data":"|8|"},{"text":"9","callback_data":"|9|"},{"text":"· 10 ·","callback_data":"|10|"},{"text":"11","callback_data":"|11|"},{"text":"12","callback_data":"|12|"},{"text":"13 ›","callback_data":"|13|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderModifyingPreviousAndNextTemplates()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(10);
+
+        $options = array(
+            'previous_template' => '< %text%',
+            'next_template'     => '%text% >',
+        );
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"1","callback_data":"|1|"},{"text":"8","callback_data":"|8|"},{"text":"9","callback_data":"|9|"},{"text":"· 10 ·","callback_data":"|10|"},{"text":"11","callback_data":"|11|"},{"text":"12 >","callback_data":"|12|"},{"text":"100 »","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+
+    public function testRenderModifiyingStringTemplate()
+    {
+        $this->setNbPages(100);
+        $this->setCurrentPage(1);
+
+        $options = array(
+            'first_page_template' => '<< %text%',
+            'last_page_template'  => '%text% >>',
+            'previous_template'   => '< %text%',
+            'next_template'       => '%text% >',
+            'container_template'  => '%pages%',
+            'page_template'       => '(%text%)',
+            'current_template'    => '| %text% |',
+        );
+
+        $this->assertRenderedView(<<<EOF
+{"inline_keyboard":[[{"text":"(| 1 |)","callback_data":"|1|"},{"text":"(2)","callback_data":"|2|"},{"text":"(3)","callback_data":"|3|"},{"text":"(4)","callback_data":"|4|"},{"text":"(5)","callback_data":"|5|"},{"text":"(6)","callback_data":"|6|"},{"text":"(7)","callback_data":"|7|"},{"text":"(8)","callback_data":"|8|"},{"text":"(9)","callback_data":"|9|"},{"text":"(10)","callback_data":"|10|"},{"text":"(11 >)","callback_data":"|11|"},{"text":"(100 >>)","callback_data":"|100|"}]]}
+EOF
+        , $this->renderView($options));
+    }
+}
