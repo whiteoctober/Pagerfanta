ebbbe5191a30cba0078640576304c2cdaf3aa485
Iesde <55848930+Iesde@users.noreply.github.com>
2019-10-11 13:35:40 -0300

    Mudada a forma do count para uma mais otimizada

--- src/Pagerfanta/Adapter/DoctrineORMAdapter.php
+++ src/Pagerfanta/Adapter/DoctrineORMAdapter.php
@@ -12,6 +12,7 @@
 namespace Pagerfanta\Adapter;
 
 use Doctrine\ORM\Tools\Pagination\Paginator as DoctrinePaginator;
+use \Doctrine\ORM\EntityManager;
 
 /**
  * DoctrineORMAdapter.
@@ -29,13 +30,16 @@ class DoctrineORMAdapter implements AdapterInterface
      * Constructor.
      *
      * @param \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder $query               A Doctrine ORM query or query builder.
-     * @param boolean                                        $fetchJoinCollection Whether the query joins a collection (true by default).
-     * @param boolean|null                                   $useOutputWalkers    Whether to use output walkers pagination mode
+     * @param \Doctrine\ORM\EntityManager|null               $em                  Entity Manager
+     * @param Boolean                                        $fetchJoinCollection Whether the query joins a collection (true by default).
+     * @param Boolean|null                                   $useOutputWalkers    Whether to use output walkers pagination mode
      */
-    public function __construct($query, $fetchJoinCollection = true, $useOutputWalkers = null)
+    public function __construct($query, $em = null, $fetchJoinCollection = true, $useOutputWalkers = null)
     {
+        $this->query = $query;
         $this->paginator = new DoctrinePaginator($query, $fetchJoinCollection);
         $this->paginator->setUseOutputWalkers($useOutputWalkers);
+        $this->em = $em;
     }
 
     /**
@@ -51,7 +55,7 @@ class DoctrineORMAdapter implements AdapterInterface
     /**
      * Returns whether the query joins a collection.
      *
-     * @return boolean Whether the query joins a collection.
+     * @return Boolean Whether the query joins a collection.
      */
     public function getFetchJoinCollection()
     {
@@ -63,7 +67,44 @@ class DoctrineORMAdapter implements AdapterInterface
      */
     public function getNbResults()
     {
-        return count($this->paginator);
+        
+        if($this->em === null || empty($this->em)){
+            return count($this->paginator);
+        } else {
+        $queryObj = $this->paginator->getQuery();
+
+//      Pega a query gerada como string sem os par�metros
+        $sql = $queryObj->getSQL();
+
+//      Cria e usa fun��o para pegar os par�metros em ordem
+        $getSqlWithParams = \Closure::bind(function($query){
+            return [$query->getSql(), $query->processParameterMappings($query->_parserResult->getParameterMappings())];
+        }, null, \Doctrine\ORM\Query::class);
+        $qparams = $getSqlWithParams($queryObj);
+
+//      Percorre pelos par�metros substituindo os '?' na query um por um
+        foreach ($qparams[1][0] as $value){
+            if(strpos($sql, '?') !== false){
+                if(is_array($value)){
+                    $sql = substr_replace($sql, implode(',', $value), strpos($sql, '?'), 1);
+                } else {
+                    $sql = substr_replace($sql, $value, strpos($sql, '?'), 1);
+                }
+            }
+        }
+
+
+//      Cria a tabela temporaria com  query tratada, faz a contagem da mesma e por fim a exclui
+        $conn = $conn = $this->em->getConnection();
+        $sql = 'CREATE TEMPORARY TABLE stage AS ' . $sql;
+        $conn->executeQuery($sql);
+        $sql = 'SELECT COUNT(1) FROM stage';
+        $total = $conn->executeQuery($sql)->fetchColumn(0);
+        $sql = 'DROP TABLE stage;';
+        $conn->executeQuery($sql);
+
+        return $total;
+        }
     }
 
     /**
